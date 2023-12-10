65eeb4e5282480bad691df724367c5e36c637882
sudhir-72744 <130700267+sudhir-72744@users.noreply.github.com>
2023-12-10 10:28:10 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (34.13%) - LeetSync

--- 910-nth-magical-number/nth-magical-number.cpp
+++ 910-nth-magical-number/nth-magical-number.cpp
@@ -2,35 +2,28 @@ class Solution {
 public:
 
     int nthMagicalNumber(int n, int a, int b) {
-
-         const int  m=1e9+7;
+        const int  m=1e9+7;
          long long g=__gcd(a,b);
          long long l=(a*b)/g;
-         auto check=[&](long long mid)
-         {
-             long long x=mid/a,y=mid/b;
-             long long k=x+y-(mid/l);
-                
-               
-                 return k<n;
-         };
 
-         long long low=1,high=1e18,ans=1;
-         while(low<=high)
-         {
-             long long mid=(low+(high-low)/2);
-             if(check(mid))
-             {
-                 ans=mid;
-                 low=mid+1;
-             }
-             else 
-              high=mid-1;
-         }
-         return (ans+1)%m;
-
-
-         
+        auto check=[&](long long mid){
+            long long x=mid/a,y=mid/b;
+            long long k=x+y-(mid/l);
+            return k<n;
+        };
+        long long s=1,e=1e18,ans=1;
+        while(s<=e){
+            long long mid=s+(e-s)/2;
+            if(check(mid)){
+                ans=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        ans++;
+        return ans%m;
         
     }
 };
