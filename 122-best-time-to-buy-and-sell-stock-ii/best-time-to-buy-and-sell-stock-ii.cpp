f4a39bf04d7fcea480e8dbf62a00b4b07253d553
sudhir-72744 <130700267+sudhir-72744@users.noreply.github.com>
2023-12-20 20:36:49 +0530

    Time: 3 ms (90.08%) | Memory: 13.3 MB (64.56%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
 
-    int solve(int n,bool flag, vector<int> &p){
-        if(n==p.size())return 0;
+    // int solve(int n,bool flag, vector<int> &p){
+    //     if(n==p.size())return 0;
         
-        if(flag){
-            return max(-p[n]+solve(n+1,!flag,p),solve(n+1,flag,p));
-        }
-        return max(p[n]+solve(n+1,!flag,p),solve(n+1,flag,p));
-    }
+    //     if(flag){
+    //         return max(-p[n]+solve(n+1,!flag,p),solve(n+1,flag,p));
+    //     }
+    //     return max(p[n]+solve(n+1,!flag,p),solve(n+1,flag,p));
+    // }
+    // int maxProfit(vector<int>& p) {
+    //     int n=p.size();
+    //     // return solve(0,1,p);
+
+    //     int dp[n+1][2];
+    //     memset(dp,0,sizeof(dp));
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int flag=1;flag>=0;flag--){
+    //             if(flag){
+    //                 dp[i][flag]= max(-p[i]+dp[i+1][!flag],dp[i+1][flag]);
+    //             }
+    //             else dp[i][flag]=max(p[i]+dp[i+1][!flag],dp[i+1][flag]);
+    //         }
+    //     }
+    // return dp[0][1];
+    // }
     int maxProfit(vector<int>& p) {
         int n=p.size();
-        // return solve(0,1,p);
-
-        int dp[n+1][2];
-        memset(dp,0,sizeof(dp));
-        for(int i=n-1;i>=0;i--){
-            for(int flag=1;flag>=0;flag--){
-                if(flag){
-                    dp[i][flag]= max(-p[i]+dp[i+1][!flag],dp[i+1][flag]);
-                }
-                else dp[i][flag]=max(p[i]+dp[i+1][!flag],dp[i+1][flag]);
-            }
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(p[i-1]<p[i])ans+=p[i]-p[i-1];
         }
-    return dp[0][1];
+        return ans;
     }
 };
