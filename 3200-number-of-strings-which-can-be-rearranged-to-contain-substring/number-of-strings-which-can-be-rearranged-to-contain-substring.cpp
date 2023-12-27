89586a03906bd2dbf23686151320e6776c84cc93
sudhir-72744 <130700267+sudhir-72744@users.noreply.github.com>
2023-12-27 21:12:49 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (94.48%) - LeetSync

--- 3200-number-of-strings-which-can-be-rearranged-to-contain-substring/number-of-strings-which-can-be-rearranged-to-contain-substring.cpp
+++ 3200-number-of-strings-which-can-be-rearranged-to-contain-substring/number-of-strings-which-can-be-rearranged-to-contain-substring.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:
-long pow(long x, long n, long mod) {
+private:
+    long pow(long x, long n, long mod) {
         long result = 1, p = x % mod;
         while (n) {
             if (n & 1) {
@@ -11,12 +11,15 @@ long pow(long x, long n, long mod) {
         }
         return result;
     }
-   int stringCount(int n) {
+    
+public:
+    int stringCount(int n) {
         long mod = 1e9 + 7;
         return ((
-
-    pow(26, n, mod)  - 
-    (n + 75) * pow(25, n - 1, mod) + (2 * n + 72) * pow(24, n - 1, mod)  - (n + 23) * pow(23, n - 1, mod)) % mod + mod) % mod;
-
+            + pow(26, n, mod)
+            - (n + 75) * pow(25, n - 1, mod)
+            + (2 * n + 72) * pow(24, n - 1, mod)
+            - (n + 23) * pow(23, n - 1, mod)
+        ) % mod + mod) % mod;
     }
 };
